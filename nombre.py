4cd5a52441eb4120d0cd8b1a833c6602d8164952
kary <kairy159@gmail.com>
2015-05-24 14:29:09 -0500

    se agregaron nuevos comandos

--- nombre.py
+++ nombre.py
@@ -1,3 +1,8 @@
 name= raw_input ('Introduce tu nombre completo: ')
-print 'hola'+name
+nl=len(name)
+p=name.split()
+np=len(p)
+
+print 'hola'+name+'tu nombre tiene '+ nl +'letras, incluyendo espacios, y tiene '+ np + 'palabras'
+
 
